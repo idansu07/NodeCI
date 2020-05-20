8413c8c9c3c43eb399fba7b77fa20bdb2d951f96
Idan <idansu07@gmail.com>
2020-05-20 15:52:26 +0300

    Travis config

---
+++ services/cache.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+
+const keys = require('../config/keys')
+
+const client = redis.createClient(keys.redisUrl)
+client.hget = util.promisify(client.hget)
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.cache = function(options = {}){
+    this.useCache = true
+    this.hashkey = JSON.stringify(options.key || '')
+    return this
+}
+
+mongoose.Query.prototype.exec = async function () {  
+    if(!this.useCache) return exec.apply(this,arguments)  
+    const key = JSON.stringify({...this.getQuery() ,collection: this.mongooseCollection.name })
+    const cacheValue = await client.hget(this.hashkey,key)
+    if(cacheValue){
+        const doc = JSON.parse(cacheValue)
+        return Array.isArray(doc) 
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc)
+    }
+
+    const result = await exec.apply(this,arguments)
+    client.hset(this.hashkey,key,JSON.stringify(result))
+    return result
+}
+
+module.exports = {
+    clearHash(hashkey){
+        client.del(JSON.stringify(hashkey))
+    }
+};
